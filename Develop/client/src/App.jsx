cc8f8e3688f1c4434baf95653d8e60524b8eff0e
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-03-27 11:34:18 -0400

    apollo

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,19 @@
+import React from 'react';
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
 
 import Navbar from './components/Navbar';
+import { apolloClient } from '../../../ApolloClient';
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={apolloClient}>
+      <Router>
       <Navbar />
       <Outlet />
-    </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
