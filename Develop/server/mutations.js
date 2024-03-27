076a3ef7ce005ca4e390b9e715071d84363c5b00
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-03-27 11:20:07 -0400

    mutations.js and queries.js

---
+++ Develop/server/mutations.js
@@ -0,0 +1,47 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      token
+      user {
+        id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: BookInput!) {
+    saveBook(input: $input) {
+      id
+      title
+      author
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      id
+      title
+      author
+    }
+  }
+`;
