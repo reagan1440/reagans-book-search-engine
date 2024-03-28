e76b7263b7f515515bfeffb1a64f23f07ef80a30
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-03-27 21:04:24 -0400

    server.js production

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -14,12 +14,9 @@ module.exports = {
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
-
     if (!token) {
       return res.status(400).json({ message: 'You have no token!' });
     }
