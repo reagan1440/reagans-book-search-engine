3c5e640613193d6cc2bcee4a22ca8e042da4809c
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-03-27 22:22:00 -0400

    add user in signupform.jsx

--- Develop/client/src/components/LoginForm.jsx
+++ Develop/client/src/components/LoginForm.jsx
@@ -1,14 +1,16 @@
 // see SignupForm.js for comments
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from "@apollo/client";
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const[login] = useMutation(LOGIN_USER)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -35,6 +37,9 @@ const LoginForm = () => {
       const { token, user } = await response.json();
       console.log(user);
       Auth.login(token);
+      const {data} = await login ({
+        variables: {...userFormData}
+      })
     } catch (err) {
       console.error(err);
       setShowAlert(true);
